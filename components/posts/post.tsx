e693e3e3efeb20516889985443cf8e19b3e458bb
Angel Zuniga <angel.zuna@icloud.com>
2023-12-25 18:47:24 -0500

    Removed title option from post

--- components/posts/post.tsx
+++ components/posts/post.tsx
@@ -2,7 +2,7 @@ import { Post } from "@prisma/client";
 import { db } from "@/lib/db";
 import Markdown from "react-markdown";
 export const PostItem = async ({ postData }: { postData: Post }) => {
-    const { title, tagId, content } = postData;
+    const { tagId, content } = postData;
     const tag = await db.postTag.findUnique({
         where: {
             id: tagId,
