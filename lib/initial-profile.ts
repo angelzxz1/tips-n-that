3f1459b65983c633066d653e047fb15cd33050bb
Angel Zuniga <angel.zuna@icloud.com>
2023-12-31 02:09:04 -0500

    added console.log in user in init user to check some logs

--- lib/initial-profile.ts
+++ lib/initial-profile.ts
@@ -4,12 +4,15 @@ import { User } from "@prisma/client";
 
 export const initialProfile = async (): Promise<User> => {
     const user = await currentUser();
-    if (!user) return redirectToSignIn();
 
+    if (!user) return redirectToSignIn();
+    console.log("User Found");
     const profile = await db.user.findUnique({
         where: { userId: user.id },
     });
-    if (!profile)
+
+    if (!profile) {
+        console.log("Profile Not Found");
         return await db.user.create({
             data: {
                 userId: user.id,
@@ -23,5 +26,7 @@ export const initialProfile = async (): Promise<User> => {
                 imageUrl: user.imageUrl,
             },
         });
+    }
+    console.log("Profile Found and returned");
     return profile;
 };
